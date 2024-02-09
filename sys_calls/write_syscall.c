8f84109ff7395ed9982435475791f1ee5aa7a83b
bandarikarthik <93322915+bandarikarthik@users.noreply.github.com>
2024-02-09 22:20:15 +0530

    Update write_syscall.c

--- sys_calls/write_syscall.c
+++ sys_calls/write_syscall.c
@@ -2,4 +2,18 @@
 This file has write system call functinality it will print the text to terminal 
 */
 
+#include <unistd.h>
 
+int main(int argc, char* argv[]){
+
+unsigned int  fd = 1;  //value 1 file descriptor,output to shell/terminal
+char *buffer = "\"write system call\"\r\n";
+int i,size;
+
+for(i=0;buffer[i]!='\0';i++,size++); 
+  
+write(fd,buffer,size);      //prints buffer string to shell/terminal
+
+return 0;
+
+}
