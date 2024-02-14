7934735708485f3cffcb0534d42291811cb97101
bandarikarthik <93322915+bandarikarthik@users.noreply.github.com>
2024-02-14 10:52:02 +0530

    Update execve.c

--- sys_calls/execve.c
+++ sys_calls/execve.c
@@ -1,1 +1,21 @@
+//File name myexecve.c
 
+//gcc myexecve.c -o myexecve
+
+// learn more by exploring :  man 2 execve 
+
+#include<unistd.h>
+#include<stdio.h>
+
+int main(int argc, const char *argv[]){
+
+char *const newargv[]={"/bin/ls","-al", NULL}; 
+
+printf("before exec\n");
+execve("/bin/ls",newargv,NULL);  // ls will be loaded in current process adress space of a program
+								//also ls command will executed and list the files in current directory.
+printf("after exec\n"); 
+
+return 0;
+
+}
